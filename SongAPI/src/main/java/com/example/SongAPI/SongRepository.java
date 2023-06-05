20ab6e89661de1b0479c4cef41903650efe1140c
johannes420 <99396135+johannes420@users.noreply.github.com>
2023-06-05 21:37:59 +0200

    Delete SongAPI directory

--- SongAPI/src/main/java/com/example/SongAPI/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SongAPI;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SongRepository extends MongoRepository<SongApp, String> {
-}
